7d82a8b11ebda5f21f725fa01ba80b46e1946191
ayuansari06 <ayuansari062gmail.com>
2024-09-02 10:45:34 +0800

    updated

--- app/Controllers/C_Perpustakaan.php
+++ app/Controllers/C_Perpustakaan.php
@@ -28,7 +28,7 @@ class C_Perpustakaan extends BaseController
             'buku_favorit' => $buku_favorit
         ];
 
-        return view('Perpustakaan/inputdata', $data);
+        return view('perpustakaan/inputdata', $data);
     }
 
     public function saveDataBuku()
@@ -176,7 +176,7 @@ class C_Perpustakaan extends BaseController
 
     public function tambahData()
     {
-        return view('Perpustakaan/inputData');
+        return view('perpustakaan/inputData');
     }
     public function delete($id) 
     {
@@ -280,7 +280,7 @@ class C_Perpustakaan extends BaseController
         ];
 
         // Menampilkan view dengan data yang telah disiapkan
-        return view('Perpustakaan/inputData', $data);
+        return view('perpustakaan/inputData', $data);
     }
 
     
