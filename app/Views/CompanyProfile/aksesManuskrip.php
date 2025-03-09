92a0ffe824e366e9a731267586a5ba6326c52ff4
shatahibrizi <shatahibrizi@gmail.com>
2025-03-09 11:10:01 +0800

    samakan ukuran gambar berita

--- app/Views/CompanyProfile/aksesManuskrip.php
+++ app/Views/CompanyProfile/aksesManuskrip.php
@@ -1,146 +1,146 @@
 <?= $this->extend('template/v_template'); ?>
 
-<?= $this-> section('content'); ?>
+<?= $this->section('content'); ?>
 
 <div class="container-fluid">
     <!-- Page Heading -->
     <!-- <h1 class="h3 mb-4 text-gray-800">Data Petugas Terdaftar</h1> -->
 
     <!-- <button class="btn btn-primary mb-3">Tambah data</button> -->
-   
+
     <div class="modal fade" id="tambahKegiatan" tabindex="-1" aria-labelledby="tambahKegiatan" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title fs-5" id="tambahKegiatan">Data Hak Akses Manuskrip</h4>
-                <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
-            </div>
-            <div class="modal-body">
-                <form action="/saveManuskrip" method="post" enctype="multipart/form-data" id="form">
-                <div class="row mb-2">
-                    <label for="email" class="col-sm-3 col-form-label">Judul</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control" id="recipient-name" name="judul">
-                    </div>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title fs-5" id="tambahKegiatan">Data Hak Akses Manuskrip</h4>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
                 </div>
-                <div class="row mb-2">
-                    <label for="email" class="col-sm-3 col-form-label">Tanggal</label>
-                    <div class="col-sm-9">
-                        <input type="date" class="form-control" id="recipient-name" name="tanggal">
-                    </div>
-                </div>
-                <div class="row mb-2">
-                    <label for="email" class="col-sm-3 col-form-label">link</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control" id="recipient-name" name="link">
-                    </div>
-                </div>
-                
-                <div class="row mb-2">
-                    <label for="foto" class="col-sm-3 col-form-label">Foto</label>
-                        <div class="col-sm-2">
-                            <img src="/img/default.jpg" alt="" class="img-thumbnail img-preview">
-                        </div>
-                        <div class="col-sm-7">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input form-control" id="gambar" name="foto" onchange="previewImg('gambar')">
-                                <label class="custom-file-label" for="customFile">Gambar Maksimal 2 Mb</label>
-                                <?php if (!empty($data_publikasi['foto'])): ?>
-                                    <div class="my-2">
-                                        <p>Foto Saat Ini:</p>
-                                        <img src="<?= base_url('img/publikasi/' . $data_publikasi['foto']); ?>" alt="Foto Petugas" width="100">
-                                    </div>
-                                <?php endif; ?>
+                <div class="modal-body">
+                    <form action="/saveManuskrip" method="post" enctype="multipart/form-data" id="form">
+                        <div class="row mb-2">
+                            <label for="email" class="col-sm-3 col-form-label">Judul</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="recipient-name" name="judul">
                             </div>
                         </div>
+                        <div class="row mb-2">
+                            <label for="email" class="col-sm-3 col-form-label">Tanggal</label>
+                            <div class="col-sm-9">
+                                <input type="date" class="form-control" id="recipient-name" name="tanggal">
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <label for="email" class="col-sm-3 col-form-label">link</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="recipient-name" name="link">
+                            </div>
+                        </div>
+
+                        <div class="row mb-2">
+                            <label for="foto" class="col-sm-3 col-form-label">Foto</label>
+                            <div class="col-sm-2">
+                                <img src="/img/default.jpg" alt="" class="img-thumbnail img-preview">
+                            </div>
+                            <div class="col-sm-7">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input form-control" id="gambar" name="foto" onchange="previewImg('gambar')">
+                                    <label class="custom-file-label" for="customFile">Gambar Maksimal 2 Mb</label>
+                                    <?php if (!empty($data_publikasi['foto'])): ?>
+                                        <div class="my-2">
+                                            <p>Foto Saat Ini:</p>
+                                            <img src="<?= base_url('img/publikasi/' . $data_publikasi['foto']); ?>" alt="Foto Petugas" width="100">
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
                 </div>
-                
-                <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Simpan</button>
+                </form>
             </div>
-            </div>
-            </form>
+
         </div>
-        
     </div>
-    </div>
-    
+
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Data Hak Akses User</h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" >
-                                    <thead >
-                                        <tr>
-                                            <th style="text-align: center;">No</th>
-                                            <th style="text-align: center;">Nama</th>
-                                            <th style="text-align: center;">Pekerjaan</th> 
-                                            <th style="text-align: center;">Instansi</th> 
-                                            <th style="text-align: center;">Medsos</th> 
-                                            <th style="text-align: center;">Whatsapp</th>
-                                            <th style="text-align: center;">Email</th>
-                                            <th style="text-align: center;">Password</th>
-                                            <th style="text-align: center;">Keperluan</th>
-                                            
-                                            <th style="text-align: center;">Hak Akses</th>
-                                            
-                                        </tr>
-                                    </thead>
-                                    
-                                    <tbody >
-                                    <?php 
-                                        $no=1;
-                                        foreach($user as $u): ?>  
-                                        <tr>
-                                            <!-- <td style="text-align: center;">1</td> -->
-                                            <!-- <td style="text-align: center;"><img src="" alt="" style="width: 60px;"></td> -->
-                                            <td style="text-align: center;"><?= $no++; ?></td>
-                                            <td style="text-align: center;"><?= $u['nama']; ?></td>
-                                            <td style="text-align: center;"><?= $u['pekerjaan']; ?></td>
-                                            <td style="text-align: center;"><?= $u['instansi']; ?></td>
-                                            <td style="text-align: center;"><?= $u['medsos']; ?></td>
-                                            <td style="text-align: center;"><?= $u['wa']; ?></td>
-                                            <td style="text-align: center;"><?= $u['email']; ?></td>
-                                            <td style="text-align: center;"><?= $u['password']; ?></td>
-                                            <td style="text-align: center;"><?= $u['keperluan']; ?></td>
-                                            
-                                            <td style="text-align: center;">
-                                            
-                                                <?php if (session()->get('level') == 'Admin' ): ?> 
-                                                    <form action="/acceptedUpdate" method="post">
-                                                        <input type="hidden" name="id_user" value="<?= $u['id_user']; ?>">
-                                                        <div class="btn-group">
-                                                            <button type="button" class="btn btn-sm  btn-<?php echo ($u['accepted'] == 'Diterima') ? 'success' : (($u['accepted'] == 'Menunggu') ? 'warning' : 'danger'); ?> btn-update-status dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                                <?php echo ($u['accepted'] == 'Diterima') ? 'Diterima' : (($u['accepted'] == 'Menunggu') ? 'Menunggu' : 'Ditolak'); ?>
-                                                            </button>
-                                                            <div class="dropdown-menu">
-                                                                <button class="dropdown-item status-option" type="submit" name="accepted" value="Diterima">Diterima</button>
-                                                                <button class="dropdown-item status-option" type="submit" name="accepted" value="Menunggu">Menunggu</button>
-                                                                <button class="dropdown-item status-option" type="submit" name="accepted" value="Ditolak">Ditolak</button>
-                                                            </div>
-                                                        </div>
-                                                    </form>
-                                                <?php endif; ?>
-                                                
-                                            </td>
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Hak Akses User</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th style="text-align: center;">No</th>
+                            <th style="text-align: center;">Nama</th>
+                            <th style="text-align: center;">Pekerjaan</th>
+                            <th style="text-align: center;">Instansi</th>
+                            <th style="text-align: center;">Medsos</th>
+                            <th style="text-align: center;">Whatsapp</th>
+                            <th style="text-align: center;">Email</th>
+                            <th style="text-align: center;">Password</th>
+                            <th style="text-align: center;">Keperluan</th>
 
-                                        </tr>
-                                        <?php endforeach; ?> 
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                            <th style="text-align: center;">Hak Akses</th>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        foreach ($user as $u): ?>
+                            <tr>
+                                <!-- <td style="text-align: center;">1</td> -->
+                                <!-- <td style="text-align: center;"><img src="" alt="" style="width: 60px;"></td> -->
+                                <td style="text-align: center;"><?= $no++; ?></td>
+                                <td style="text-align: center;"><?= $u['nama']; ?></td>
+                                <td style="text-align: center;"><?= $u['pekerjaan']; ?></td>
+                                <td style="text-align: center;"><?= $u['instansi']; ?></td>
+                                <td style="text-align: center;"><?= $u['medsos']; ?></td>
+                                <td style="text-align: center;"><?= $u['wa']; ?></td>
+                                <td style="text-align: center;"><?= $u['email']; ?></td>
+                                <td style="text-align: center;"><?= $u['password']; ?></td>
+                                <td style="text-align: center;"><?= $u['keperluan']; ?></td>
+
+                                <td style="text-align: center;">
+
+                                    <?php if (session()->get('level') == 'Admin'): ?>
+                                        <form action="/acceptedUpdate" method="post">
+                                            <input type="hidden" name="id_user" value="<?= $u['id_user']; ?>">
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-sm  btn-<?php echo ($u['accepted'] == 'Diterima') ? 'success' : (($u['accepted'] == 'Menunggu') ? 'warning' : 'danger'); ?> btn-update-status dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <?php echo ($u['accepted'] == 'Diterima') ? 'Diterima' : (($u['accepted'] == 'Menunggu') ? 'Menunggu' : 'Ditolak'); ?>
+                                                </button>
+                                                <div class="dropdown-menu">
+                                                    <button class="dropdown-item status-option" type="submit" name="accepted" value="Diterima">Diterima</button>
+                                                    <button class="dropdown-item status-option" type="submit" name="accepted" value="Menunggu">Menunggu</button>
+                                                    <button class="dropdown-item status-option" type="submit" name="accepted" value="Ditolak">Ditolak</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    <?php endif; ?>
+
+                                </td>
+
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
 
 </div>
 
 <script async src="https://cdn.jsdelivr.net/npm/es-module-shims@1/dist/es-module-shims.min.js" crossorigin="anonymous"></script>
-    <script type="importmap">
+<script type="importmap">
     {
       "imports": {
         "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/popper.min.js",
@@ -148,10 +148,10 @@
       }
     }
     </script>
-    <script type="module">
-      import * as bootstrap from 'bootstrap'
+<script type="module">
+    import * as bootstrap from 'bootstrap'
 
-      new bootstrap.Popover(document.getElementById('popoverButton'))
-    </script>
+    new bootstrap.Popover(document.getElementById('popoverButton'))
+</script>
 
 <?= $this->endSection(); ?>
--- app/Views/landingPage/berita2.php
+++ app/Views/landingPage/berita2.php
@@ -1,149 +1,139 @@
 <?= $this->extend('landingPage/template baru'); ?>
 
-<?= $this-> section('content'); ?>
+<?= $this->section('content'); ?>
 
 <!-- start banner Area -->
 <section class="about-banner relative">
 	<div class="overlay overlay-bg"></div>
-		<div class="container">				
-			<div class="row d-flex align-items-center justify-content-center">
-				<div class="about-content col-lg-12">
-					<h1 class="text-white">
-								Berita		
-					</h1>	
-					<p class="text-white link-nav"><a href="/home">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="/berita2"> Berita</a></p>
-				</div>	
+	<div class="container">
+		<div class="row d-flex align-items-center justify-content-center">
+			<div class="about-content col-lg-12">
+				<h1 class="text-white">
+					Berita
+				</h1>
+				<p class="text-white link-nav"><a href="/home">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="/berita2"> Berita</a></p>
 			</div>
 		</div>
+	</div>
 </section>
-<!-- End banner Area -->	
+<!-- End banner Area -->
 
 
-			<!-- Start post-content Area -->
-			<section class="post-content-area pt-60">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-8 posts-list">
-							 
-							<div class="single-post row d-flex">
+<!-- Start post-content Area -->
+<section class="post-content-area pt-60">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 posts-list">
 
-								<?php 
-								foreach($dataBerita as $b):
-								if ($b['type'] == 'Narasi'): ?>  
-								<div class="col-lg-4 col-md-12 my-2">
-									<button class="genric-btn primary small"> 
-										<div><?= $b['jenisBerita']; ?></div>
-										<div></div>
-									</button>
-									<div class="feature-img mb-2">
-										<img class="img-fluid"  src="<?= base_url("img/berita/". $b['foto']); ?>" alt="">
-									</div>
-									<a class="posts-title "  href="<?= base_url("/lihatberita2/{$b['id_berita']}"); ?>"><h5 style="padding-bottom: 6pt;"><?= $b['judul']; ?></h5></a>
-									<p><i class="fa-solid fa-calendar-days" style="padding-right: 4pt;"></i><?= $b['tanggal']; ?></p>
-									<p class="excert"><?= $b['isi_pendek']; ?></p>
-									<a href="<?= base_url("/lihatberita2/{$b['id_berita']}"); ?>" class="primary-btn">Baca Berita</a>
+				<div class="single-post row d-flex">
+
+					<?php
+					foreach ($dataBerita as $b):
+						if ($b['type'] == 'Narasi'): ?>
+							<div class="col-lg-4 col-md-12 my-2">
+								<button class="genric-btn primary small">
+									<div><?= $b['jenisBerita']; ?></div>
+									<div></div>
+								</button>
+								<div class="feature-img mb-2 embed-responsive embed-responsive-4by3">
+									<img class="embed-responsive-item" style="object-fit: cover;" src="<?= base_url("img/berita/" . $b['foto']); ?>" alt="">
 								</div>
-								<?php endif; ?>
-                				<?php endforeach; ?>
+								<a class="posts-title " href="<?= base_url("/lihatberita2/{$b['id_berita']}"); ?>">
+									<h5 style="padding-bottom: 6pt;"><?= $b['judul']; ?></h5>
+								</a>
+								<p><i class="fa-solid fa-calendar-days" style="padding-right: 4pt;"></i><?= $b['tanggal']; ?></p>
+								<p class="excert"><?= $b['isi_pendek']; ?></p>
+								<a href="<?= base_url("/lihatberita2/{$b['id_berita']}"); ?>" class="primary-btn">Baca Berita</a>
 							</div>
-							
-							
-
-
-						</div>
-						<div class="col-lg-4 sidebar-widgets">
-							<div class="widget-wrap">
-								<div class="single-sidebar-widget post-category-widget">
-									<h4 class="category-title">Kategori Berita</h4>
-									<ul class="cat-list">
-										
-										<li>
-											<a href="/beritaKategori2/Pendidikan" class="d-flex justify-content-between">
-												<p>Pendidikan</p>
-												<!-- <p>59</p> -->
-											</a>
-										</li>
-										<li>
-											<a href="/beritaKategori2/Sosial Masyarakat" class="d-flex justify-content-between">
-												<p>Sosial Masyarakat</p>
-												<!-- <p>29</p> -->
-											</a>
-										</li>
-										<li>
-											<a href="/beritaKategori2/Sejarah dan Budaya" class="d-flex justify-content-between">
-												<p>Sejarah dan Budaya</p>
-												<!-- <p>15</p> -->
-											</a>
-										</li>
-										<li>
-											<a href="/beritaKategori2/Pemerintahan" class="d-flex justify-content-between">
-												<p>Pemerintahan</p>
-												<!-- <p>09</p> -->
-											</a>
-										</li>
-										<li>
-											<a href="/beritaKategori2/Pariwisata" class="d-flex justify-content-between">
-												<p>Pariwisata</p>
-												<!-- <p>09</p> -->
-											</a>
-										</li>
-										<li>
-											<a href="/berita2" class="d-flex justify-content-between">
-												<p>Semua Kategori</p>
-												
-											</a>
-										</li>
-										
-									</ul>
-								</div>	
-								
-								<div class="single-sidebar-widget popular-post-widget">
-									<h4 class="popular-title">Berita Terkait</h4>
-									<!-- <a href="#" class="genric-btn primary-border my-2">  Regional  </a>
-									<a href="#" class="genric-btn primary-border my-2">  Nasional  </a> -->
-									<form method="POST" action="">
-									<div id="kategoriBerita" class="col-lg-12 d-flex">
-										<button   class="genric-btn primary-border small my-2 mx-2 <?= $kategoriBerita == 'Regional' ? 'active' : '' ?>" name="kategoriBerita" value="Regional" >Regional</button>
-										<button  class="genric-btn primary-border small my-2  mx-2 <?= $kategoriBerita == 'Nasional' ? 'active' : '' ?>" name="kategoriBerita" value="Nasional">Nasional</button>
-									</div>
-									</form>
-
-									<div class="popular-post-list">
-										<?php 
-										foreach($berita as $b):
-										if ($b['type'] == 'Link'): ?>
-										<div class="single-post-list d-flex flex-row align-items-center">
-											<div class="thumb">
-												<!-- <img class="img-fluid" src="img/blog/pp1.jpg" alt=""> -->
-											</div>
-											<div class="details">
-												<a href="<?= $b['isi'] ?>" target="_blank"><h6>> <?= $b['judul']; ?></h6></a>
-												<p><?= $b['tanggal']; ?></p>
-											</div>
-										</div>
-										<?php endif; ?>
-               							<?php endforeach; ?>
-														
-									</div>
-									<?php if (count($berita) >= 3 && !$lihatSemua): ?>
-										<div>
-											<a href="?kategoriBerita=<?= $kategoriBerita ?>&lihatSemua=1" style="color: #850000;">Lihat semua. . .</a>
-										</div>
-									<?php endif; ?>
-								</div>
-
-
-
-
-
-								
-								
-							</div>
-						</div>
-
+						<?php endif; ?>
+					<?php endforeach; ?>
+				</div>
+			</div>
+			<div class="col-lg-4 sidebar-widgets">
+				<div class="widget-wrap">
+					<div class="single-sidebar-widget post-category-widget">
+						<h4 class="category-title">Kategori Berita</h4>
+						<ul class="cat-list">
+							<li>
+								<a href="/beritaKategori2/Pendidikan" class="d-flex justify-content-between">
+									<p>Pendidikan</p>
+									<!-- <p>59</p> -->
+								</a>
+							</li>
+							<li>
+								<a href="/beritaKategori2/Sosial Masyarakat" class="d-flex justify-content-between">
+									<p>Sosial Masyarakat</p>
+									<!-- <p>29</p> -->
+								</a>
+							</li>
+							<li>
+								<a href="/beritaKategori2/Sejarah dan Budaya" class="d-flex justify-content-between">
+									<p>Sejarah dan Budaya</p>
+									<!-- <p>15</p> -->
+								</a>
+							</li>
+							<li>
+								<a href="/beritaKategori2/Pemerintahan" class="d-flex justify-content-between">
+									<p>Pemerintahan</p>
+									<!-- <p>09</p> -->
+								</a>
+							</li>
+							<li>
+								<a href="/beritaKategori2/Pariwisata" class="d-flex justify-content-between">
+									<p>Pariwisata</p>
+									<!-- <p>09</p> -->
+								</a>
+							</li>
+							<li>
+								<a href="/berita2" class="d-flex justify-content-between">
+									<p>Semua Kategori</p>
+								</a>
+							</li>
+						</ul>
 					</div>
-				</div>	
-			</section>
-			<!-- End post-content Area -->
 
-<?= $this->endSection(); ?>  
+					<div class="single-sidebar-widget popular-post-widget">
+						<h4 class="popular-title">Berita Terkait</h4>
+						<!-- <a href="#" class="genric-btn primary-border my-2">  Regional  </a>
+									<a href="#" class="genric-btn primary-border my-2">  Nasional  </a> -->
+						<form method="POST" action="">
+							<div id="kategoriBerita" class="col-lg-12 d-flex">
+								<button class="genric-btn primary-border small my-2 mx-2 <?= $kategoriBerita == 'Regional' ? 'active' : '' ?>" name="kategoriBerita" value="Regional">Regional</button>
+								<button class="genric-btn primary-border small my-2  mx-2 <?= $kategoriBerita == 'Nasional' ? 'active' : '' ?>" name="kategoriBerita" value="Nasional">Nasional</button>
+							</div>
+						</form>
+
+						<div class="popular-post-list">
+							<?php
+							foreach ($berita as $b):
+								if ($b['type'] == 'Link'): ?>
+									<div class="single-post-list d-flex flex-row align-items-center">
+										<div class="thumb">
+											<!-- <img class="img-fluid" src="img/blog/pp1.jpg" alt=""> -->
+										</div>
+										<div class="details">
+											<a href="<?= $b['isi'] ?>" target="_blank">
+												<h6>> <?= $b['judul']; ?></h6>
+											</a>
+											<p><?= $b['tanggal']; ?></p>
+										</div>
+									</div>
+								<?php endif; ?>
+							<?php endforeach; ?>
+
+						</div>
+						<?php if (count($berita) >= 3 && !$lihatSemua): ?>
+							<div>
+								<a href="?kategoriBerita=<?= $kategoriBerita ?>&lihatSemua=1" style="color: #850000;">Lihat semua. . .</a>
+							</div>
+						<?php endif; ?>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+<!-- End post-content Area -->
+
+<?= $this->endSection(); ?>
